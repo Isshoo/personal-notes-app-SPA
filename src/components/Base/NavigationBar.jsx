01b81ac2f7e93d5447af48f30554db395aa4217c
Tods <algy25ng@gmail.com>
2025-01-09 00:57:56 +0800

    useLocation for responsive navigation button style

--- src/components/Base/NavigationBar.jsx
+++ src/components/Base/NavigationBar.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiHome, FiPlusCircle } from 'react-icons/fi';
+import { Link, useLocation } from 'react-router-dom';
 
 function NavigationBar() {
+  const location = useLocation();
+
   return (
     <nav className="navigation-bar">
       <ul className="navigation">
         <li>
-          <Link id="allNotesBtn" to="/">
+          <Link
+            id="allNotesBtn"
+            className={`nav-button ${location.pathname === '/' ? 'active' : ''}`}
+            to="/"
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link id="archivedListBtn" to="/archived">
+          <Link
+            id="archivedListBtn"
+            className={`nav-button ${location.pathname === '/archived' ? 'active' : ''}`}
+            to="/archived"
+          >
             Archived
           </Link>
         </li>
